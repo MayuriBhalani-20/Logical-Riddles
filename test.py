33f872ab0ce3eed3bee39667fd507e9cc410c5fc
MayuriBhalani-20 <mayuripatel1520@gmial.com>
2022-11-09 09:48:12 +0530

    this file is just for testing different code

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-def test(string1, discount):
-    punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+sentence = "there are $1 $2 and 5$ candies $ma2 in the shop"
+discount = int(input('Discount value:'))
+s = sentence.split(" ")
+#len(s)
+for x in range(len(s)):
+            if s[x][0] =='$':
+              price = int(s[x][1:])
+              new_price = ''
+              new_price+=s[x][0]
+              dis = (price*discount) / 100
+              apply = price - dis
+              apply = "{:.2f}".format(apply)
+              s[x] = new_price+apply
+              print(s[x])
+            else:
+              continue
